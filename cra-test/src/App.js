fb0fba4573426ac8cd0624f51678236102977100
bita5000 <das2ome@gmail.com>
2023-01-19 00:34:26 +0900

    테스트 코드 / 코드 분할

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import smallImg from './small.png';
 import bigImg from './big.png';
+import TodoList from './TodoList';
 
 function App() {
   return (
     <div className="App">
       <img src={bigImg} alt="big" />
       <img src={smallImg} alt="small" />
+      <TodoList />
     </div>
   );
 }
